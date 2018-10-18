24e1cd462ccac3d30079e5535d88cc9dd37c1543
¨KrisKorrel¨ <kriskorrel@gmail.com>
2018-10-18 19:47:09 +0200

    Add sparsemax function

---
+++ sparsemax.py
@@ -0,0 +1,70 @@
+"""Sparsemax activation function.
+
+Pytorch implementation of Sparsemax function from:
+-- "From Softmax to Sparsemax: A Sparse Model of Attention and Multi-Label Classification"
+-- André F. T. Martins, Ramón Fernandez Astudillo (http://arxiv.org/abs/1602.02068)
+"""
+
+from __future__ import division
+
+import torch
+import torch.nn as nn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class Sparsemax(nn.Module):
+    """Sparsemax function."""
+
+    def forward(self, input):
+        """Forward function.
+
+        Input should 2-dimensional.
+
+        Args:
+            input (torch.Tensor): [batch_size x number_of_logits] Input tensor
+
+        Returns:
+            torch.Tensor: [batch_size x number_of_logits] Output tensor
+
+        """
+        dim = 1
+        number_of_logits = input.size(dim)
+
+        # Translate input by max for numerical stability
+        input = input - torch.max(input, dim=dim, keepdim=True)[0].expand_as(input)
+
+        # Sort input in descending order.
+        # (NOTE: Can be replaced with linear time selection method described here:
+        # http://stanford.edu/~jduchi/projects/DuchiShSiCh08.html)
+        zs = torch.sort(input=input, dim=dim, descending=True)[0]
+        range = torch.range(start=1, end=number_of_logits, device=device).view(1, -1)
+        range = range.expand_as(zs)
+
+        # Determine sparsity of projection
+        bound = 1 + range * zs
+        cumulative_sum_zs = torch.cumsum(zs, dim)
+        is_gt = torch.gt(bound, cumulative_sum_zs).type(input.type())
+        k = torch.max(is_gt * range, dim, keepdim=True)[0]
+
+        # Compute threshold function
+        zs_sparse = is_gt * zs
+
+        # Compute taus
+        taus = (torch.sum(zs_sparse, dim, keepdim=True) - 1) / k
+        taus = taus.expand_as(input)
+
+        # Sparsemax
+        self.output = torch.max(torch.zeros_like(input), input - taus)
+
+        return self.output
+
+    def backward(self, grad_output):
+        """Backward function."""
+        dim = 1
+
+        nonzeros = torch.ne(self.output, 0)
+        sum = torch.sum(grad_output * nonzeros, dim=dim) / torch.sum(nonzeros, dim=dim)
+        self.grad_input = nonzeros * (grad_output - sum.expand_as(grad_output))
+
+        return self.grad_input
